1eb60d8c8cb05acd1ba81d51e410c0fae14306a4
Jaysukh1994 <Jayshukh1994@gmail.com>
2018-01-31 14:50:48 +0530

    Added comment

--- MyCode/src/Test.java
+++ MyCode/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-		//this is trial for class
+		//this is trial for class added comment
 		Test1 test1 = new Test1();
 		test1.print();
 	}
